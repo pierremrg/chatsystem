34e0a9e2eb3c5314b0cec0116a285e5173e93acf
Pierre Marigo <pierre.mrg@gmail.com>
2018-12-07 18:02:45 +0100

    Server

--- src/client_server/Server.java
+++ src/client_server/Server.java
@@ -1,7 +1,52 @@
 package client_server;
 
-public class Server {
+import java.io.IOError;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
+public class Server extends Thread {
 
+	public Server() {
+		super();
+	}
 
+	public void run() {
+
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		
+		try {
+			serverSocket = new ServerSocket(5000);
+			socket = serverSocket.accept();
+	
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+	
+			out.println(new Date().toString());
+		}
+		catch(Exception e) {
+			System.out.println("Erreur sur le serveur");
+		}
+		
+		finally {
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (Exception e) {
+					System.out.println("Erreur fermeture socket");
+				}
+			}
+	
+			if (serverSocket != null) {
+				try {
+					serverSocket.close();
+				} catch (Exception e) {
+					System.out.println("Erreur fermeture serverSocket");
+				}
+			}
+		}
+		
+	}
 }
