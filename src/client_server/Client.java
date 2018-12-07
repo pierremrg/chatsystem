1b6cc50d8778f832b1653ba2c84d8c3a551f7a56
TomEighty15 <thomas.vialars@hotmail.fr>
2018-12-07 18:02:37 +0100

    Creation thread client

--- src/client_server/Client.java
+++ src/client_server/Client.java
@@ -1,7 +1,41 @@
 package client_server;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
 
+public class Client extends Thread {
 
-
+	public Client() {
+		super();
+	}
+	
+	public void run() {
+		Socket client = null;
+		
+		try {
+			client = new Socket(InetAddress.getLocalHost(), 5000);
+			System.out.println("Connected...");
+			
+			BufferedReader in_data = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			
+			String data = null;
+			
+			while((data = in_data.readLine()) != "-1") {
+				System.out.println("Message : " + data);
+			}
+		} catch (Exception e) {
+			System.out.println("Erreur client...");
+		} finally {			
+			System.out.println("Deconnecting...");
+			try {
+				client.close();
+			} catch (Exception e) {
+				System.out.println("Erreur deconnexion");
+			}
+			System.out.println("Deconnected...");	
+		}	
+	}
 }
--- src/client_server/Main.java
+++ src/client_server/Main.java
@@ -3,8 +3,8 @@ package client_server;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Client client = new Client();
+		client.start();
 	}
 
 }
