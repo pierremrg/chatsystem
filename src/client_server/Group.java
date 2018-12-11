71c2e937af8dc02ef64e4f2362ff4bf4de0143cd
Pierre Marigo <pierre.mrg@gmail.com>
2018-12-11 14:47:45 +0100

    Fin du corps des classes Group et User

--- src/client_server/Group.java
+++ src/client_server/Group.java
@@ -1,12 +1,50 @@
 package client_server;
 
+import java.util.ArrayList;
+
 public class Group {
 	
 	private int id;
-	private int memberId;
+	private ArrayList<User> members;
 	
-	public Group(int id) {
+	/**
+	 * Création d'un groupe
+	 * @param id ID du groupe
+	 * @param members Liste des membres du groupe
+	 */
+	public Group(int id, ArrayList<User> members) {
 		this.id = id;
+		this.members = members;
+	}
+	
+	/**
+	 * Retourne l'ID du groupe
+	 * @return l'ID du groupe
+	 */
+	public int getID() {
+		return id;
+	}
+	
+	/**
+	 * Retourne les membres du groupe
+	 * @return les membres du groupe
+	 */
+	public ArrayList<User> getMembers() {
+		return members;
+	}
+	
+	/**
+	 * Teste si un utilisateur est membre de ce groupe
+	 * @param member L'utilisateur à tester
+	 * @return True si l'utilisateur est dans le groupe, False sinon
+	 */
+	public boolean isMember(User member) {
+		for(User m : members) {
+			if(m == member)
+				return true;
+		}
+		
+		return false;
 	}
 	
 	
--- src/client_server/User.java
+++ src/client_server/User.java
@@ -3,38 +3,75 @@ package client_server;
 public class User {
 
 	private int id;
-	private String username;
+	private String username; // TODO : crypter ?
 	private String password;
 	private String ip;
 	private int port;
 
+	/**
+	 * Crée un utilisateur
+	 * @param username Le username de l'utilisateur
+	 * @param password Le password de l'utilisateur
+	 */
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
+	
+	/**
+	 * Retourne l'ID de l'utilisateur
+	 * @return l'ID de l'utilisateur
+	 */
+	public int getID() {
+		return id;
+	}
 
+	/**
+	 * Retourne le username de l'utilisateur
+	 * @return le username de l'utilisateur
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Modifie le username de l'utilisateur
+	 * @param username Le nouveau username
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
+	/**
+	 * Retourne le mot de passe de l'utilisateur
+	 * @return le mot de passe de l'utilisateur
+	 */
+	public String getPassword() {
+		return password;
+	}
 
+	/**
+	 * Modifie le password de l'utilisateur
+	 * @param password Le nouveau password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/**
+	 * Retourne l'IP de l'utilisateur
+	 * @return l'IP de l'utilisateur
+	 */
 	public String getIP() {
 		return ip;
 	}
 
+	/**
+	 * Retourne le port de l'utilisateur
+	 * @return le port de l'utilisateur
+	 */
 	public int getPort() {
 		return port;
-	}
-
-	public void connect(String ip, int port) {
-		// TODO
 	}
 
 }
