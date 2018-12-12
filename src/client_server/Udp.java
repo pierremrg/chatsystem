2005e8395dac5f7b499878660c6e78b3e49c5d8e
ThomasVialars <thomas.vialars@hotmail.fr>
2018-12-12 12:14:24 +0100

    classe Message, Controller, Udp

---
+++ src/client_server/Udp.java
@@ -0,0 +1,86 @@
+package client_server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Udp extends Thread {
+	private Controller controller;
+	private DatagramSocket socket;
+	
+	public Udp(Controller controller) {
+		super();
+		this.controller = controller;
+		
+		try {
+			this.socket = new DatagramSocket(5000);
+		} catch (SocketException e) {
+			System.out.println("Erreur socket udp 1");
+			e.printStackTrace();
+		}
+		try {
+			socket.setBroadcast(true);
+		} catch (SocketException e) {
+			System.out.println("Erreur socket udp 2");
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendUdpMessage(String message, String ip) {
+		InetAddress address = null;
+		try {
+			address = InetAddress.getByName(ip);
+		} catch (UnknownHostException e) {
+			System.out.println("Erreur adresse IP");
+			e.printStackTrace();
+		}
+		int port = 5000;
+		byte[] buffer = message.getBytes();
+		DatagramPacket out = new DatagramPacket(buffer, buffer.length, address, port);
+		try {
+			socket.send(out);
+		} catch (IOException e) {
+			System.out.println("Erreur socketsend udp");
+			e.printStackTrace();
+		}
+	}
+	
+	public void addConnectedUser(int idUser) {
+		controller.receiveConnection(idUser);
+	}
+	
+	public void removeConnectedUser(int idUser) {
+		controller.receiveDeconnection(idUser);
+	}
+	
+	public void run() {
+		byte[] buffer = new byte[256];
+		DatagramPacket in = new DatagramPacket(buffer, buffer.length);
+		try {
+			socket.receive(in);
+		} catch (IOException e) {
+			System.out.println("Erreur socket");
+			e.printStackTrace();
+		}
+	
+		String messageRecu = new String(in.getData(), 0, in.getLength());		
+		int statutConnexion = Integer.parseInt(messageRecu.substring(0, 1));
+		int idUser = Integer.parseInt(messageRecu.substring(2));
+		
+		
+		if(statutConnexion == 0) {
+			removeConnectedUser(idUser);
+		}
+		else if(statutConnexion == 1) {
+			addConnectedUser(idUser);
+			this.sendUdpMessage("2 " + this.controller.getUser().getID(), in.getAddress().toString());
+		} else if(statutConnexion == 2) {
+			addConnectedUser(idUser);
+		}
+		
+	}
+	
+}
